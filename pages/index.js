0c1ab12ac29532d0a28a43607fdb9ad506ec1e37
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2022-09-28 11:01:00 +0800

    add meta image

--- pages/index.js
+++ pages/index.js
@@ -2,86 +2,110 @@ import Head from "next/head"
 import Image from "next/image"
 import ImageGrid from "../components/ImageGrid"
 
-const menusLeft = [
-  {
-    name: "webpemda",
-    href: "https://www.sinjaikab.go.id/v4",
-  },
-  {
-    name: "lpse",
-    href: "http://lpse.sinjaikab.go.id/eproc4",
-  },
-  {
-    name: "dprd",
-    href: "http://dprd.sinjaikab.go.id",
-  },
-  {
-    name: "barugasulsel",
-    href: "https://sinjaikab.go.id/sip",
-  },
-  {
-    name: "kotaku",
-    href: "http://kotaku.pu.go.id",
-  },
-  {
-    name: "rdtr",
-    href: "https://rdtrsinjai.com",
-  },
-  {
-    name: "simpelmi",
-    href: "http://dpmptsp.sinjaikab.go.id",
-  },
-  {
-    name: "pad",
-    href: "http://e-pad.sinjaikab.go.id/?t=1538968364&u=",
-  },
-]
-
-const menusRight = [
-  {
-    name: "enikda",
-    href: "http://enikda.sinjaikab.go.id/v3",
-  },
-  {
-    name: "webgis",
-    href: "http://webgis.sinjaikab.go.id",
-  },
-  {
-    name: "sirup",
-    href: "https://sirup.lkpp.go.id/sirup/ro/rekap/klpd/D401",
-  },
-  {
-    name: "simpeg",
-    href: "http://apps.sinjaikab.go.id/simpeg/user/login",
-  },
-  {
-    name: "email",
-    href: "https://sinjaikab.go.id/webmail",
-  },
-  {
-    name: "ppid",
-    href: "http://ppidkab.sinjaikab.go.id",
-  },
-  {
-    name: "jdih",
-    href: "http://jdih.sinjaikab.go.id",
-  },
-  {
-    name: "e-office",
-    href: "http://apps.sinjaikab.go.id/office",
-  },
-]
-
 export default function Home() {
+  const title = "Pemerintah Kabupaten Sinjai"
+  const description = "Website Resmi Pemerintah Kabupaten Sinjai"
+  const url = "https://sinjaikab.vercel.app"
+  const image = `${url}/sinjaikab-meta.png`
+
+  const menusLeft = [
+    {
+      name: "webpemda",
+      href: "https://www.sinjaikab.go.id/v4",
+    },
+    {
+      name: "lpse",
+      href: "http://lpse.sinjaikab.go.id/eproc4",
+    },
+    {
+      name: "dprd",
+      href: "http://dprd.sinjaikab.go.id",
+    },
+    {
+      name: "barugasulsel",
+      href: "https://sinjaikab.go.id/sip",
+    },
+    {
+      name: "kotaku",
+      href: "http://kotaku.pu.go.id",
+    },
+    {
+      name: "rdtr",
+      href: "https://rdtrsinjai.com",
+    },
+    {
+      name: "simpelmi",
+      href: "http://dpmptsp.sinjaikab.go.id",
+    },
+    {
+      name: "pad",
+      href: "http://e-pad.sinjaikab.go.id/?t=1538968364&u=",
+    },
+  ]
+
+  const menusRight = [
+    {
+      name: "enikda",
+      href: "http://enikda.sinjaikab.go.id/v3",
+    },
+    {
+      name: "webgis",
+      href: "http://webgis.sinjaikab.go.id",
+    },
+    {
+      name: "sirup",
+      href: "https://sirup.lkpp.go.id/sirup/ro/rekap/klpd/D401",
+    },
+    {
+      name: "simpeg",
+      href: "http://apps.sinjaikab.go.id/simpeg/user/login",
+    },
+    {
+      name: "email",
+      href: "https://sinjaikab.go.id/webmail",
+    },
+    {
+      name: "ppid",
+      href: "http://ppidkab.sinjaikab.go.id",
+    },
+    {
+      name: "jdih",
+      href: "http://jdih.sinjaikab.go.id",
+    },
+    {
+      name: "e-office",
+      href: "http://apps.sinjaikab.go.id/office",
+    },
+  ]
+
   return (
     <div className="font-sans">
       <Head>
-        <title>Pemerintah Kabupaten Sinjai</title>
-        <meta
-          name="description"
-          content="Website Resmi Pemerintah Kabupaten Sinjai"
-        />
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Primary Meta Tags */}
+        <title>{title}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+
+        {/* Google / Search Engine Tags */}
+        <meta itemProp="name" content={title} />
+        <meta itemProp="description" content={description} />
+        <meta itemProp="image" content={image} />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={image} />
       </Head>
 
       <main className="h-screen flex justify-center items-center bg-gradient-to-b from-slate-100 to-slate-500 relative">
